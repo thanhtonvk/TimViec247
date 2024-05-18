b8580060bfddfe41c175570c45d088a7c1112b46
tondt <tondt@dyno.vn>
2024-05-18 21:44:18 +0700

    add

---
+++ app/src/main/java/com/utehy/timviec247/activities/CompanyDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.utehy.timviec247.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.utehy.timviec247.R;
+
+public class CompanyDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_details);
+    }
+}
